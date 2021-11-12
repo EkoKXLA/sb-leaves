d8feb6bb2a2c2883d55ae1ea8d3a262c0ba73874
EkoKXLA <89838541+EkoKXLA@users.noreply.github.com>
2021-11-12 16:54:03 +0700

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+				SISFO Sofiabella <a href="wa.me/6281317936493" target="_blank"><span>developed by </span>© 13strd 2021</a>
+			</div>
---
+++ admin/includes/left_sidebar.php
@@ -0,0 +1,66 @@
+<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="admin_dashboard.php">
+				<img src="../vendors/images/deskapp-logo-svg.png" alt="" class="dark-logo">
+				<img src="../vendors/images/sblong.png" alt="" class="light-logo">
+			</a>
+			<div class="close-sidebar" data-toggle="left-sidebar-close">
+				<i class="ion-close-round"></i>
+			</div>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li class="dropdown">
+						<a href="admin_dashboard.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-house-1"></span><span class="mtext">Dashboard</span>
+						</a>
+						
+					</li>
+					<li>
+						<a href="department.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-calendar1"></span><span class="mtext">Divisi</span>
+						</a>
+					</li>
+					<li>
+						<a href="leave_type.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-calendar1"></span><span class="mtext">Tipe Cuti</span>
+						</a>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-library"></span><span class="mtext">Staff</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add_staff.php">New Staff</a></li>
+							<li><a href="staff.php">Manage Staff</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-apartment"></span><span class="mtext"> Cuti </span>
+						</a>
+						<ul class="submenu">
+							<li><a href="apply_leave.php">Apply Cuti</a></li>
+							<li><a href="leaves.php">Semua Cuti</a></li>
+							<li><a href="pending_leave.php">Pending Cuti</a></li>
+							<li><a href="approved_leave.php">Approved Cuti</a></li>
+							<li><a href="rejected_leave.php">Rejected Cuti</a></li>
+						</ul>
+					</li>
+
+					<li>
+						<div class="dropdown-divider"></div>
+					</li>
+					<li>
+						<div class="sidebar-small-cap">Extra</div>
+					</li>
+					<li>
+						<a href="https://103.10.170.98/" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-edit-2"></span><span class="mtext">Sisfo Sofiabella</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
